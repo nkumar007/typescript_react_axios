b158740c112cebe030ed59acf345e6b55529d261
nkumar007 <naveen.mbuff98@gmail.com>
2023-03-23 14:59:24 +0530

    making our http service completely generic

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,30 +1,8 @@
-import apiClient from "./api-client";
+import create from "./http-service";
 
 export interface User {
   id: number;
   name: string;
 }
 
-class userService {
-  getUsers() {
-    const controller = new AbortController();
-    const request = apiClient.get<User[]>("/users", {
-      signal: controller.signal,
-    });
-    return { request, cancel: () => controller.abort() };
-  }
-
-  deleteUser(user: User) {
-    return apiClient.delete("/users/" + user.id);
-  }
-
-  addUser(newUser: User) {
-    return apiClient.post("/users/", newUser);
-  }
-
-  updateUser(user: User, updatedUser: User) {
-    return apiClient.patch("/users/" + user.id, updatedUser);
-  }
-}
-
-export default new userService();
+export default create("/users");
